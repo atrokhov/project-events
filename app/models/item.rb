204d17d08b22ec6e0ae84bd7a5012251ba260491
Arthur Atrokhov <aatrokhov@gmail.com>
2018-06-15 11:02:44 +0600

    Function of searching was create

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
 	has_many :item_categories, dependent: :destroy
 	has_many :categories, through: :item_categories
 	has_many :comments, dependent: :destroy
+	def self.search(search)
+  	where("name ILIKE ?", "%#{search}%") 
+  	where("description ILIKE ?", "%#{search}%")
+	end
 end
